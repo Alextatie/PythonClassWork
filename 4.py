5f3919235d60651d70e8d201b66a362b7428cbc6
Alextatie <57321474+Alextatie@users.noreply.github.com>
2019-11-13 13:08:59 +0200

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-# GCD with Euclidean algorithm
-print("Enter two integers:")
-a, b, c = int(input()), int(input()), 0
-print("GCD(", a, ",", b, ")=")
-while b != 0:
-    c = a
-    a = b
-    b = c % b
-print("gcd=", a)
